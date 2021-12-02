1d51fc46c5faf993d20637364004cc4f02fdb0e6
SaulACC <saul_chavira2000@hotmail.com>
2021-12-02 15:54:57 -0700

    Test

--- src/server.js
+++ src/server.js
@@ -233,6 +233,7 @@ app.put('/api/paymentUpdate',protectedRoutes, (req, res) => {
 
 app.put('/api/upload', protectedRoutes,(req, res)=>{ /// user uploads image to payment
     const {uId, pImage} = req.body;
+    res.json(pImage)
     let q = `UploadImage ${pImage}, ${uId}`;
     deadpool(req, res, q);
     res.json('Payment done, waiting for confirmation.');
